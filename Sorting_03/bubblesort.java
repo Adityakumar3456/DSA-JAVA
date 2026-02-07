ee16c326a1aae125365dd92f82627278af5898a7
Adityakumar3456 <95834489+Adityakumar3456@users.noreply.github.com>
2026-02-07 16:39:19 +0000

    Sorting

---
+++ Sorting_03/bubblesort.java
@@ -0,0 +1,31 @@
+package Sorting_03;
+
+public class bubblesort {
+    public static void sort(int arr[]){
+        for(int turn = 0; turn<arr.length-1; turn++){
+            for(int j = 0; j<arr.length-1; j++){
+                if(arr[j] > arr[j+1]){
+                    //swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+
+    }
+
+    public static void print(int arr[]){
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String [] args){
+        int arr[] = {10,20,1,2,4};
+
+        sort(arr);
+        print(arr);
+    }
+}
---
+++ Sorting_03/selectionsort.java
@@ -0,0 +1,32 @@
+package Sorting_03;
+
+public class selectionsort {
+
+    public static void sort(int arr[]){
+        for(int i = 0; i<arr.length-1; i++){
+            int minpos = i;
+            for(int j = i+1; j<arr.length; j++){
+                if(arr[minpos]> arr[j]){
+                    minpos = j;
+                }
+            }
+            //swap
+            int temp = arr[minpos];
+            arr[minpos] = arr[i];
+            arr[i] = temp;
+
+        }
+    }
+
+    public static void print(int arr[]){
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String [] args){
+        int arr[] = {10,20,1,2,4};
+
+        sort(arr);
+        print(arr);
+    }
+}
