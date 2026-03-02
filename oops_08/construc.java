5836c122f4559cf65cfc32ce8efe8458fa2f0456
Adityakumar3456 <95834489+Adityakumar3456@users.noreply.github.com>
2026-03-02 12:50:26 +0000

    OOPs fundamental done

---
+++ oops_08/construc.java
@@ -0,0 +1,16 @@
+package oops_08;
+public class construc {
+    public static void main(String []args){
+        Student s1 = new Student();
+        
+    }
+}
+
+class Student{
+    String name;
+    int roll;
+
+    Student(){   //constructor is made
+        System.out.println("This is constructor");
+    }
+}
---
+++ oops_08/thiss.java
@@ -0,0 +1,18 @@
+package oops_08;
+
+public class thiss {
+    public static void main(String[] args){
+    nikama n1 = new nikama("Adi");
+    
+    }
+}
+
+class nikama{
+    String name = "He";
+    String surname;
+
+    nikama(String name){
+        this.name = name;
+        System.out.println(name);
+    }
+}
